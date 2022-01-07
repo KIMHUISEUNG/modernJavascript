ecdb7883fa15c21eafa172bac5810b2fca1fc1a1
hatso <mmorpg3636@naver.com>
2022-01-07 17:59:48 +0900

    first commit

---
+++ JS/modern.js
@@ -0,0 +1,18 @@
+var str1 = "var";
+let str2 = "let";
+
+console.log(this.str1);
+console.log(this.str2);
+
+function run() {
+  str1 = "var2";
+  str2 = "let2";
+  console.log(this.str1);
+  console.log(this.str2);
+  console.log(str1);
+  console.log(str2);
+}
+
+run();
+
+console.log(str1);
